fe7164e5473839285da524d4547dd9609bfe5e8e
Jim Lynch <jim@jam.sessionsnet.org>
2014-05-30 11:55:23 -0700

    just a plain list streamer

--- list-streamer.scm
+++ list-streamer.scm
@@ -1,59 +1,26 @@
 ;; makes a streamer for a grouped list (as output by rests-to-groupings)
 ;;
-;; returns list:
-;; (item in-group-p last-in=group-p)
+;; returns next note from list
 
-;; data structure streamout
-
-;constructor
-(define (mk-streamout note in-group? last-in-group?)
-  (list note in-group? last-in-group?)
-)
-
-; selectors
-(define streamout-note car)
-(define streamout-in-group? cadr)
-(define streamout-last-in-group? caddr)
-
-(define (mk-note-streamer note-list)
-  (let 
-    (
-      (state note-list)
-      (group '())
-    )
+(define (mk-list-streamer the-list)
+  (let
+    ((state the-list))
+    
     (lambda ()
-      (let ;; binding for local var
+      (let
         ((prev '()))
+        
         (cond
-          ((pair? group)
-            (set! prev (car group))
-            (set! group (cdr group))
-            
-            (mk-streamout prev #t (null? group))
-          )
-          ((pair? state)
-            (cond 
-              ((pair? (car state))
-                (set! group (car state))
-                (set! state (cdr state))
-              
-                (set! prev (car group))
-                (set! group (cdr group))
-              
-                (mk-streamout prev #t (null? group))
-              )
-              (else
-                (set! prev (car state))
-                (set! state (cdr state))
-              
-                (mk-streamout prev #f #f)
-              )
-            )
-          )
-          (else
+          ((null? state)
             '()
           )
-        )
+          (else
+            (set! prev (car state))
+            (set! state (cdr state))
+            
+            prev
+          )
+        )          
       )
     )
   )
