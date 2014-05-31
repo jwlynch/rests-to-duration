d4152004af9bc6aab7af3c55f7a5b7797c254dc3
Jim Lynch <jim@jam.sessionsnet.org>
2014-05-31 15:47:50 -0700

    rename test case to sample-1, samp-1, result-1; add second test case using rests over split to see if it makes a difference whether a rest or tied note is over the split

--- testing.scm
+++ testing.scm
@@ -3,7 +3,7 @@
 (load "apply-measure-patterns.scm")
 
 (define 
-  sample 
+  sample-1
   (list
     (mk-rest 1/8)
     (mk-rest 1/8)
@@ -16,6 +16,22 @@
   )
 )
 
-(define samp-1 (rests-to-durations sample))
-(define result (apply-measure-pattern samp-1 '(1/2 1/2))
+(define 
+  sample-2
+  (list
+    (mk-rest 1/8)
+    (mk-rest 1/8)
+    (mk-rest 1/8)
+    (mk-rest 1/8)
+    (mk-rest 1/8)
+    (mk-note 1/8 'b 1)
+    (mk-note 1/8 'c 1)
+    (mk-note 1/8 'd 1)
+  )
+)
+
+(define samp-1 (rests-to-durations sample-1))
+(define samp-2 (rests-to-durations sample-2))
+(define result-1 (apply-measure-pattern samp-1 '(1/2 1/2)))
+(define result-2 (apply-measure-pattern samp-2 '(1/2 1/2)))
 
