741547616cc4ed22982da59a29e51230a6ca7b1f
Jim Lynch <jim@jam.sessionsnet.org>
2014-06-03 13:49:06 -0700

    OK start, problems to solve still

--- measure-pattern-test.scm
+++ measure-pattern-test.scm
@@ -1,12 +1,15 @@
 (define (measure-pattern-test measure pattern)
   (let*
     (
-      (measure-streamer )
-      (pattern-streamer )
+      (measure-streamer (mk-list-streamer measure))
+      (pattern-streamer (mk-list-streamer pattern))
+      (note-or-rest (measure-streamer))
+      (pattern-value (pattern-streamer))
     )
     
     (measure-pattern-test-parser
       measure-streamer
+      note-or-rest
       pattern-streamer
       pattern-value
       #t
@@ -17,8 +20,42 @@
 (define 
   (measure-pattern-test-parser
     measure-streamer
+    note-or-rest
     pattern-streamer
     pattern-value
     bool-result
+  )
+  
+  (cond
+    ((null? note-or-rest)
+      (if (and (= pattern-value 0) (null? pattern-value)) ;; not possible, fixit
+        bool-result
+        #f
+      )
+    )
+    ((= 0 pattern-value)
+      (and
+        bool-result
+        (measure-pattern-test-parser
+          measure-streamer
+          note-or-rest
+          pattern-streamer
+          (pattern-streamer)
+          bool-result
+        )
+      )
+    )
+    (else
+      (and
+        bool-result
+        (measure-pattern-test-parser
+          measure-streamer
+          (measure-streamer)
+          pattern-streamer
+          (- pattern-value (note-duration note-or-rest))
+          bool-result
+        )
+      )
+    )
   )
 )
