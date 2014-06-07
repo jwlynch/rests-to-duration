29d06898d670837e0279d5bb824f15bd96fb177d
Jim Lynch <jim@jam.sessionsnet.org>
2014-06-07 15:50:47 -0700

    add parameters, make it collect sums of series of 1-bits

--- note-durations.scm
+++ note-durations.scm
@@ -37,14 +37,83 @@
 
 ;; TODO: rewrite this to find serieses of 1-bits in duration list
 ;; (at the moment it's the same as the above)
-(define (split-duration-adj-one-bits-star duration-list denom pwr2)
+(define 
+  (split-duration-adj-one-bits-star 
+    duration-list 
+    denom 
+    pwr2
+    last-was-1?
+    num-ones
+    first-one
+    total-of-run
+  )
+  
   (cond
-    ((null? duration-list) '())
-    ((char=? #\1 (car duration-list))
-      (cons (/ pwr2 denom)
-            (split-duration-adj-one-bits-star (cdr duration-list) denom (* 2 pwr2))
+    ((null? duration-list) 
+      (if last-was-1? ;; then add element to the list
+        (cons
+          (list total-of-run first-one (- num-ones 1)) ;; (dur note numdots)
+          '()
+        )
+        '()
       )
     )
-    (else ( split-duration-adj-one-bits-star (cdr duration-list) denom (* 2 pwr2) ))
+    ((char=? #\1 (car duration-list)) ;; collect this pwr of 2
+      (split-duration-adj-one-bits-star 
+        (cdr duration-list) 
+        denom 
+        (* 2 pwr2)
+        #t
+        (+ num-ones 1)
+        (if (not last-was-1?) (/ pwr2 denom) first-one)
+        (+ total-of-run (/ pwr2 denom))
+      )
+    )
+    (else 
+      (let
+        ((split-output
+          (split-duration-adj-one-bits-star 
+            (cdr duration-list) 
+            denom 
+            (* 2 pwr2) 
+            #f
+            0
+            0
+            0
+          )
+        ))
+        
+        (if last-was-1?
+	  (cons
+	    (list total-of-run first-one (- num-ones 1)) ;; (dur note numdots)
+	    split-output
+	  )
+	  split-output
+        )
+      )
+    )
   )
 )
+
+(define (split-duration-dots duration)
+  (let*
+    (
+      (numer (numerator duration))
+      (denom (denominator duration))
+      (duration-list (duration-list numer))
+    )
+    
+    (reverse 
+      (split-duration-adj-one-bits-star 
+        duration-list 
+        denom
+        1
+        #f
+        0
+        0
+        0
+      )
+    )
+  )
+)
+
