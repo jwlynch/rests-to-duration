d4fdba5a95a3b7d21112fac2bf7f9ed78f8a81a1
Jim Lynch <jim@jam.sessionsnet.org>
2014-06-10 16:56:49 -0700

    start working on the func

--- lilypond-output.scm
+++ lilypond-output.scm
@@ -1,2 +1,17 @@
 ;; This will do the final translation from the note.scm form to the form suitable for lilypond input.
 
+;; this will be "hard-wired" for simple quadruple 4/4 time
+
+(define (lilypond-output measure)
+  (cond
+    ((null? measure) '())
+    ((eq? (car measure) 'TIE) "~")
+    ((note-isrest? (car measure) 
+    )
+    ((note-isnote? (car measure)
+    )
+    (else
+      (lilypond-output (cdr measure))
+    )
+  )
+)
