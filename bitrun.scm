e618d8af79f6d8995709caa184b36ecc4dd859d5
Jim Lynch <jim@jam.sessionsnet.org>
2014-06-10 20:19:38 -0700

    bitrun: output of split-durations-dots, data structure defs

---
+++ bitrun.scm
@@ -0,0 +1,23 @@
+;; this defines bitrun data structure, made out of a list
+;;
+;; used for note durations to indicate the basic note and the number of dots to follow
+
+;; constructor
+(define (mk-bitrun total base-dur num-dots)
+  (list 'bitrun total base-dur num-dots)
+)
+
+;; recognizer
+(define (is-bitrun it)
+  (cond
+    ((not (pair? it)) #f)
+    ((not (eq? (car it) 'bitrun)) #f
+    (else #t)
+  )
+)
+
+;; selectors
+(define bitrun-total cadr)
+(define bitrun-basicdur caddr)
+(define bitrun-numdots cadddr)
+
