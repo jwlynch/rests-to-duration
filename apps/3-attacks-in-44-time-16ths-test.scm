78ec9638fa7f5735b03a9f4b8a0c1c18c55a3bb6
Jim Lynch <jim@jam.sessionsnet.org>
2014-06-19 01:13:06 -0700

    let's see it without simple-quadruple process

---
+++ apps/3-attacks-in-44-time-16ths-test.scm
@@ -0,0 +1,26 @@
+(load "tool-attribs.scm")
+
+(load (string-append prefix "arrangement.scm"))
+(load (string-append prefix "rests-to-durations.scm"))
+(load (string-append prefix "simple-quadruple-time.scm"))
+(load (string-append prefix "result-writing.scm"))
+(load (string-append prefix "note.scm"))
+(load (string-append prefix "lilypond-output.scm"))
+
+(define arrs (arrangements 16 (list (list (mk-note 1/16 'g 0) 3) (list (mk-rest 1/16) 13))))
+
+(define legatoized (map rests-to-durations arrs))
+
+;;(define quadized (map simple-quadruple legatoized))
+
+(define result (map lilypond-output legatoized))
+
+(display "\\version \"2.14.2\"\n\n")
+
+(display "{\n")
+(display "  \\clef bass\n\n")
+
+(disp-result-items result)
+
+(display "}\n")
+
